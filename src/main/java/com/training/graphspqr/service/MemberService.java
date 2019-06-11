c90874d4fe9f62295375b1f32833fb79930e480a
leafy <icarus8050@naver.com>
2019-06-12 00:11:03 +0900

    mutation example

--- src/main/java/com/training/graphspqr/service/MemberService.java
+++ src/main/java/com/training/graphspqr/service/MemberService.java
@@ -2,6 +2,8 @@ package com.training.graphspqr.service;
 
 import com.training.graphspqr.domain.Member;
 import com.training.graphspqr.repository.MemberRepository;
+import io.leangen.graphql.annotations.GraphQLContext;
+import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,11 @@ public class MemberService {
     @GraphQLQuery(name = "select")
     public Member findByIdMemberQuery(Long id) {
         return memberRepository.findById(id).orElse(null);
+    }
+
+    @GraphQLMutation(name = "update")
+    public Member updateMember(Member member) {
+        return memberRepository.saveAndFlush(member);
     }
 
 }
